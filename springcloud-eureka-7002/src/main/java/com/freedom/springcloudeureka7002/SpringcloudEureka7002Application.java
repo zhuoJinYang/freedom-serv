5b3b2d8ddaf892c337bcba455197ef869ec41a1a
zjy <956541455@qq.com>
2022-11-17 08:41:11 +0800

    增加eureka集群

---
+++ springcloud-eureka-7002/src/main/java/com/freedom/springcloudeureka7002/SpringcloudEureka7002Application.java
@@ -0,0 +1,16 @@
+package com.freedom.springcloudeureka7002;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+// @EnableEurekaServer 服务端的启动类，可以接受别人注册进来~
+@EnableEurekaServer
+@SpringBootApplication
+public class SpringcloudEureka7002Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringcloudEureka7002Application.class, args);
+    }
+
+}
